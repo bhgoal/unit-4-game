d9168284dcd41f6606ddd7d58ddb74c1923e5617
gorbasho <37026147+gorbasho@users.noreply.github.com>
2018-04-23 22:03:28 -0500

    logic done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,14 @@
-//Create four character options, each with 3 stats: Health, attack power, counterattack power.
-//    Create img for each, link img to click, click = var(playerCharacter)
-//    Swap div elements to hide remaining characters, show those 3 characters in Enemies to Attack instead
-//    Click on remaining 3 characters = var(enemyChosen)
-//    Swap div elements to hide remaining characters, show enemyChosen in Fight section
-//    enemiesRemaining = 3
-//    Attack button function:
-//        numberAttacks ++
-//        enemy Health -= player Attack power
-//        player Health -= enemy Counterattack power
-//        player Attack power = (numberAttacks + 1) * player Attack power
-
-//    if (enemy Health == 0)
-//        Swap div to hide enemyChosen
-//        Prompt new enemy selection
-//        return to Attack button stage
-//        enemiesRemaining --
-
-//    if (player Health == 0)
-//        Player loses
-
-//    if (enemiesRemaining == 0)
-//        Player wins]
-
-var health = {
-    scav: 50,
-    bear: 75,
-    usec: 75,
-    prapor: 100,
-};
-
+// Define base stats for each of 4 characters.
 var stats = {
     scav: {
         health: 50,
-        attackPower: 4,
+        attackPower: 4, // If this character is chosen as player character, attack power = damage dealt by player to enemies.
         counterattackPower: 6
     },
     bear: {
         health: 55,
         attackPower: 5,
-        counterattackPower: 3
+        counterattackPower: 3 // If this character is an enemy, counterattack power = damage received by player when attacking this enemy
     },
     usec: {
         health: 70,
@@ -52,8 +22,20 @@ var stats = {
     }
 };
 
+// HTML to be inserted into various page elements, to move character cards around screen.
+// Contains span element for reactive health display when dealing/receiving damage.
+var showCard = {
+    scav: '<div class="card mx-auto" id="scav" style="width: 14rem;"><img class="card-img-top" src="assets/images/scav.png" alt="Scav image cap"><div class="card-body"><h5 class="card-title">Scav - <span id="scavHealth">50</span> HP</h5></div></div>',
+    bear: '<div class="card mx-auto" id="bear" style="width: 14rem;"><img class="card-img-top" src="assets/images/bear.png" alt="Bear image cap"><div class="card-body"><h5 class="card-title">Bear - <span id="bearHealth">55</span> HP</h5></div></div>',
+    usec: '<div class="card mx-auto" id="usec" style="width: 14rem;"><img class="card-img-top" src="assets/images/usec.png" alt="Usec image cap"><div class="card-body"><h5 class="card-title">Usec - <span id="UsecHealth">70</span> HP</h5></div></div>',
+    prapor: '<div class="card mx-auto" id="prapor" style="width: 14rem;"><img class="card-img-top" src="assets/images/prapor.png" alt="Prapor image cap"><div class="card-body"><h5 class="card-title">Prapor - <span id="praporHealth">100</span> HP</h5></div></div>',
+    button: '<div class="text-center"><button type="button" id="attackButton" class="btn btn-danger btn-lg mt-5">Attack!</button></div>'
+};
+
+// Name of player character choice
 var characterChoice = "";
 
+// Number of attacks player has performed so far, to track increasing attack damage.
 var numberAttacks = 1;
 
 var playerChar = {
@@ -61,36 +43,57 @@ var playerChar = {
     attackPower: "",
     counterattackPower: ""
 };
-
 var enemyChar = {
     health: "",
     attackPower: "",
     counterattackPower: ""
 };
-
-var showCard = {
-    scav: '<div class="card" id="scav" style="width: 14rem;"><img class="card-img-top" src="assets/images/scav.png" alt="Scav image cap"><div class="card-body"><h5 class="card-title">Scav</h5></div></div>',
-    bear: '<div class="card" id="bear" style="width: 14rem;"><img class="card-img-top" src="assets/images/bear.png" alt="Bear image cap"><div class="card-body"><h5 class="card-title">Bear</h5></div></div>',
-    usec: '<div class="card" id="usec" style="width: 14rem;"><img class="card-img-top" src="assets/images/usec.png" alt="Usec image cap"><div class="card-body"><h5 class="card-title">Usec</h5></div></div>',
-    prapor: '<div class="card" id="prapor" style="width: 14rem;"><img class="card-img-top" src="assets/images/prapor.png" alt="Prapor image cap"><div class="card-body"><h5 class="card-title">Prapor</h5></div></div>',
-};
-
+// Array to track how many enemies left to defeat.
 var charactersRemain = ["scav", "bear", "usec", "prapor"];
-console.log(charactersRemain);
+
+// New game resets player/enemy character choices and stats
+function newGame() {
+    console.log("Begin newGame function");
+    characterChoice = "";
+
+    numberAttacks = 1;
+
+    playerChar = {
+        health: "",
+        attackPower: "",
+        counterattackPower: ""
+    };
+    enemyChar = {
+        health: "",
+        attackPower: "",
+        counterattackPower: ""
+    };
+    charactersRemain = ["scav", "bear", "usec", "prapor"];
+    console.log(charactersRemain);
+
+    // Hide attack button
+    $("#attackButton").css("visibility","hidden");
+
+    selectCharacter();
+}
+newGame();
+
 
 // Select character
 function selectCharacter() {
+    console.log("Begin selectCharacter function");
+
     $(".card").on("click", function() {
+        // Player clicks on a character card, choice is recorded as string into characterChoice
         characterChoice = $(this).attr("id");
-        console.log("character clicked");
-        console.log(stats[characterChoice]);
+        // Stats of selected character copied into playerChar object
         for(var k in stats.bear) {playerChar[k]=stats[characterChoice][k];}
         console.log(playerChar);
 
         // Remove chosen character from array of remaining
         charactersRemain.splice($.inArray(characterChoice, charactersRemain),1);
         console.log(charactersRemain);
-        // Move chosen character to left, others to right
+        // Move chosen character to far left of screen, others to right
         $("#leftSubCol1").html(showCard[characterChoice]);
         $("#leftSubCol2").html("");
         $("#rightSubCol1").html("");
@@ -101,20 +104,21 @@ function selectCharacter() {
         selectEnemy();
     });
 }
-selectCharacter();
 
-// Select active enemy
+// Select enemy to fight
 function selectEnemy() {
+    console.log("Begin selectEnemy function");
     $(".card").on("click", function() {
+        // Prevent player character card from being selected
         if ($(this).attr("id") != characterChoice) {
             activeEnemy = $(this).attr("id");
-            console.log("enemy clicked");
-            console.log(stats[activeEnemy]);
             for(var k in stats.bear) {enemyChar[k]=stats[activeEnemy][k];}
             console.log(enemyChar);
+
+            // Remove chosen enemy from array of remaining
             charactersRemain.splice($.inArray(activeEnemy, charactersRemain),1);
             console.log(charactersRemain);
-            // Move chosen character to left, others to right
+            // Move chosen character to left
             $("#rightSubCol1").html(showCard[activeEnemy]);
             $("#rightSubCol2").html("");
             for (i = 0; i < charactersRemain.length; i++) {
@@ -126,33 +130,48 @@ function selectEnemy() {
     });
 }
 
+// Player attacks enemy
 function attack() {
-    $("#attackButton").on("click", function() {
-        if (attackMode === true) {
-            console.log("button clicked");
-            playerChar.health -= enemyChar.attackPower;
-            enemyChar.health -= (numberAttacks * playerChar.attackPower);
-            numberAttacks ++;
-            console.log("numberAttacks: " + numberAttacks);
-            console.log("player health: " + playerChar.health);
-            console.log("enemy health: " + enemyChar.health);
-            if (playerChar.health <= 0) {
-                console.log("Game over: You died");
-                attackMode = false;
+    console.log("Begin attack function");
+    // Show attack button on screen
+    $("#leftSubCol2").html(showCard.button);
+    $("#attackButton").css("visibility","visible");
+    $("#attackButton").unbind('click').on("click", function() {
+        // Player takes damage from enemy
+        playerChar.health -= enemyChar.attackPower;
+        // Update player display
+        $(`#${characterChoice}Health`).html(playerChar.health);
+        // Player deals damage to enemy, multiplied by total number of attacks performed
+        enemyChar.health -= (numberAttacks * playerChar.attackPower);
+        // Update enemy display
+        $(`#${activeEnemy}Health`).html(enemyChar.health);
+        // Update attack count
+        numberAttacks ++;
+        console.log("numberAttacks: " + numberAttacks);
+        console.log("Player health: " + playerChar.health);
+        console.log("Enemy health: " + enemyChar.health);
+
+        // Lose condition
+        if (playerChar.health <= 0) {
+            console.log("Game over: You died");
+            $("#attackButton").css("visibility","hidden");
+        }
+
+        // When current enemy defeated
+        if (enemyChar.health <= 0) {
+            $("#attackButton").css("visibility","hidden");
+            $("#leftSubCol2").html("Enemy defeated!");
+            console.log("Enemy defeated");
+            // Clear enemy from fight area
+            $("#rightSubCol1").html("");
+            // Win condition if enemy was last one
+            if (charactersRemain.length === 0) {
+                $("#leftSubCol2").html("You survived!");
+                console.log("You win!");
+            } else { // Otherwise, return to enemy selection
+                $("#rightSubCol1").html("Select next enemy -->");
+                selectEnemy();
             }
-            if (enemyChar.health <= 0) {
-                attackMode = false;
-                console.log("Enemy defeated");
-                $("#rightSubCol1").html("");
-                if (charactersRemain.length === 0) {
-                    console.log("You win!");
-                } else {
-                    selectEnemy();
-                }
-            }
-            console.log(attackMode);
-        } else {
-            console.log("select new enemy");
         }
     });
 }
